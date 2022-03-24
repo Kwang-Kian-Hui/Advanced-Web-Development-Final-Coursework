8756986ac65768c65e008514ad78b6b1691326ac
Kwang Kian Hui <2k1h297@gmail.com>
2022-03-24 12:49:29 +0800

    serializers and apis

--- src/friends/urls.py
+++ src/friends/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from friends import views as fviews
 from django.conf.urls.static import static
 from django.conf import settings
+from . import api
 
 app_name = "friend_app"
 
@@ -13,6 +14,7 @@ urlpatterns = [
     path('friend_request/', fviews.send_friend_request, name="friend_request"),
     path('friend_request/<user_id>/', fviews.friend_request_view, name="friend_request_list"),
     path('remove_friend/', fviews.remove_friend, name="remove_friend"),
+    path('api/friendlist/<int:pk>/', api.FriendDetail.as_view(), name='friends_api'), 
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/users/urls.py
+++ src/users/urls.py
@@ -2,12 +2,15 @@ from django.urls import path
 from users import views as uview
 from django.conf.urls.static import static
 from django.conf import settings
+from . import api
 
 app_name = "user_app"
 
 urlpatterns = [
     path('<user_id>/', uview.profile_view, name="user_profile"),
     path('<user_id>/edit/', uview.edit_user_view, name="edit_user"),
+    path('api/user/', api.CreateUser.as_view(), name='create_user_api'), 
+    path('api/user/<int:pk>/', api.UserDetail.as_view(), name='user_detail_api'), 
 ]
 
 if settings.DEBUG:
